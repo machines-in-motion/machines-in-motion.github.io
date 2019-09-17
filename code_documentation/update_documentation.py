f1fb22a0388795cf49f5fcffce05b0b7c09f448c
Maximilien Naveau <maximilien.naveau@tuebingen.mpg.de>
2019-09-17 19:44:25 +0200

    [wip] add a script that uses treep in order to detect the packages to extract the documentation from

--- code_documentation/update_documentation.py
+++ code_documentation/update_documentation.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
-
 from os import path, mkdir
+import re
 import shutil
+import treep.treep_git
+import treep.files
 import rospkg
 
 def copy_doc_package(package_name):
@@ -18,5 +20,18 @@ def copy_doc_package(package_name):
         shutil.copytree(local_doc_html, local_doc)
 
 if __name__ == "__main__":
-    
-    copy_doc_package("dynamic_graph_manager")
+    ros_pack = rospkg.RosPack()
+    share_path = path.dirname(ros_pack.get_path("dynamic_graph_manager"))
+    print(share_path)
+    treep_projects = treep.files.read_configuration_files(share_path)
+
+    repos_names = treep_projects.get_repos("CORE_ROBOTICS")
+    print(repos_names)
+    for repos_name in repos_names:
+        repos_path = treep_projects.get_repo_path(repos_name)
+        repos_url = treep.treep_git.get_url(repos_path)
+        if re.search(repos_url, "*machines-in-motion*"):
+            print(repos_path)
+            print(repos_url)
+
+        
