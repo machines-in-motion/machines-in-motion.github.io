e465ad08739e57c6b3d09f51a208ca44841f339c
Bamboo build agent <bambooagent@sow-oldlady3.is.localnet>
2019-10-05 15:54:23 +0200

    [bamboo blmc_robots] update documentation

--- code_documentation/pinocchio_bullet/search/all_0.js
+++ code_documentation/pinocchio_bullet/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['get_5fforce',['get_force',['../classpython_1_1py__pinocchio__bullet_1_1wrapper_1_1PinBulletWrapper.html#ab91d7c32be024c5d90bbb7af527313fb',1,'python::py_pinocchio_bullet::wrapper::PinBulletWrapper']]]
+  ['get_5fforce',['get_force',['../classpython_1_1py__pinocchio__bullet_1_1wrapper_1_1PinBulletWrapper.html#ab91d7c32be024c5d90bbb7af527313fb',1,'python::py_pinocchio_bullet::wrapper::PinBulletWrapper']]],
+  ['get_5fstate_5fupdate_5fpinocchio',['get_state_update_pinocchio',['../classpython_1_1py__pinocchio__bullet_1_1wrapper_1_1PinBulletWrapper.html#ab4ea626824c8043f48aa3c57b7504426',1,'python::py_pinocchio_bullet::wrapper::PinBulletWrapper']]]
 ];
