4ed3176119e435e7c064f4b7b67fbf7d8520039f
MaximilienNaveau <maximilien.naveau@gmail.com>
2020-05-30 14:12:33 +0200

    debug bamboo

--- sphinx_source_files/update_doc_page.py
+++ sphinx_source_files/update_doc_page.py
@@ -31,7 +31,7 @@ def get_ros_install_share_path():
     root_path = treep.files._find_root(share_path, True)[0]
     print("root_path = ", root_path)
     if not root_path:
-        share_path = path.abspath(path.curdir())
+        share_path = path.abspath(path.curdir)
     root_path = treep.files._find_root(share_path, True)[0]
     print("root_path = ", root_path)
     if not root_path:
