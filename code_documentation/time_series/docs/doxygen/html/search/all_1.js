022dcec76c6b7c1b81b4217a0c58455647c0afa7
Bamboo build agent <bambooagent@sow-oldlady3.is.localnet>
2020-08-28 12:01:49 +0200

    [bamboo core-robotics] update documentation

--- code_documentation/time_series/docs/doxygen/html/search/all_1.js
+++ code_documentation/time_series/docs/doxygen/html/search/all_1.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['count_5fappended_5felements',['count_appended_elements',['../classtime__series_1_1TimeSeriesInterface.html#a46cf7fc73adfcc400fbf40c22aa7268f',1,'time_series::TimeSeriesInterface']]],
-  ['create_5ffollower',['create_follower',['../classtime__series_1_1MultiprocessTimeSeries.html#a17c2f98a820da37e6e292e482c27925a',1,'time_series::MultiprocessTimeSeries']]],
-  ['create_5fleader',['create_leader',['../classtime__series_1_1MultiprocessTimeSeries.html#a3ae3a4dde19c31a012cccf7ca56d7f06',1,'time_series::MultiprocessTimeSeries']]]
+  ['create_5ffollower',['create_follower',['../classtime__series_1_1MultiprocessTimeSeries.html#a81d203564cfe4255cc095b7e2ae74936',1,'time_series::MultiprocessTimeSeries']]],
+  ['create_5ffollower_5fptr',['create_follower_ptr',['../classtime__series_1_1MultiprocessTimeSeries.html#a49df57b9c60f77537af1e11fbd60a74d',1,'time_series::MultiprocessTimeSeries']]],
+  ['create_5fleader',['create_leader',['../classtime__series_1_1MultiprocessTimeSeries.html#ab0378e1f82637590e6929a426f285711',1,'time_series::MultiprocessTimeSeries']]],
+  ['create_5fleader_5fptr',['create_leader_ptr',['../classtime__series_1_1MultiprocessTimeSeries.html#ac1462ae53879411f081503275472a975',1,'time_series::MultiprocessTimeSeries']]]
 ];
--- code_documentation/time_series/docs/doxygen/html/search/all_3.js
+++ code_documentation/time_series/docs/doxygen/html/search/all_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['get_5fmax_5flength',['get_max_length',['../classtime__series_1_1MultiprocessTimeSeries.html#a83ccacdbe676a375f08716ef718d1398',1,'time_series::MultiprocessTimeSeries']]],
+  ['get_5fmax_5flength_5fand_5fstart_5findex_5ffrom_5fleader',['get_max_length_and_start_index_from_leader',['../classtime__series_1_1MultiprocessTimeSeries.html#aa832318a5ad0f4f2987b83911ea52e0e',1,'time_series::MultiprocessTimeSeries']]],
   ['get_5fstart_5ftimeindex',['get_start_timeindex',['../classtime__series_1_1MultiprocessTimeSeries.html#a049fea012e647d352c3cef9f5b9a9db1',1,'time_series::MultiprocessTimeSeries']]]
 ];
